3646ca11048349af5f775456144d56faca7df3d4
TimberTrader <tp.bowes@gmail.com>
2019-06-17 14:24:41 -0600

    working thru demos - koa middleware

--- demos/09.js
+++ demos/09.js
@@ -7,10 +7,10 @@ const one = (ctx, next) => {
   console.log('<< one');
 }
 
-const two = (ctx, next) => {
-  console.log('>> two');
-  next();
-  console.log('<< two');
+// const two = (ctx, next) => {
+//   console.log('>> two');
+//   next();
+//   console.log('<< two');
 }
 
 const three = (ctx, next) => {
@@ -20,7 +20,7 @@ const three = (ctx, next) => {
 }
 
 app.use(one);
-app.use(two);
+// app.use(two);
 app.use(three);
 
 app.listen(3000);
